46d113f21ebeabb6e834b57e03061151caa4b61b
hargtholan@gmail.com <hargtholan@gmail.com@19df2200-dbfd-8bb6-e353-256aed0257e5>
2011-03-10 20:02:56 +0000

    New front of work.

---
+++ vector.cpp
@@ -0,0 +1,110 @@
+/***************************************************************************
+ *   Copyright (C) 2010-2011 by                                            *
+ *                                                                         *
+ *   Leandro Graciá Gil                                                    *
+ *   leandro.gracia.gil@gmail.com                                          *
+ *                                                                         *
+ *   Juan V. Puertos Ahuir                                                 *
+ *   juanvi.puertos@gmail.com                                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/**
+ * \file 	feature_vector.cpp
+ * \brief 	Template class implementation for generic D-dimensional
+ *              feature vectors.
+ * \author	Leandro Graciá Gil, Puertos Ahuir Juan V.
+ * \email       juanvi.puertos@gmail.com
+*/
+
+#include <cstdlib>
+#include <new>
+#include "vector.h"
+
+/**
+ * Generic memory allocator operator for feature vector arrays.
+ * Defined so that memory-aligned specializations can be defined if required.
+ *
+ * \param size  Total size in bytes of the objects to allocate.
+ * \return Address to the new allocated memory.
+ */
+
+template <typename T, const unsigned int D, int M>
+void* Vector<T, D, M>::operator new [] (size_t size)
+{ 
+  void *p = malloc(size);
+  if (p == NULL) {
+    std::bad_alloc exception;
+    throw exception;
+  }
+  return p;
+}
+
+/**
+ * Generic memory deallocator operator for feature vector arrays.
+ * Defined as complement of operator new [] so that memory-aligned
+ * specializations can be defined if required.
+ *
+ * \param p     Pointer to the address to release.
+ */
+
+template <typename T, const unsigned int D, int M>
+void Vector<T, D, M>::operator delete [] (void *p)
+{        
+  free(p);
+}
+
+/**
+ * Check if two feature vectors are exactly equal.
+ *
+ * \param p Feature vector being compared to.
+ * \return \c true if equal, \c false otherwise.
+ */
+template <typename T, const unsigned int D, int M>
+bool Vector<T, D, M>::operator== (const Vector &p) const
+{
+  // Check that all components have the same value
+  for (unsigned int d=0; d<D; ++d)
+    if(data[d] != p.data[d])
+      return false;
+
+  return true;
+}
+
+/**
+ * Check if two feature vectors are different equal.
+ *
+ * \param p Feature vector being compared to.
+ * \return \c true if different, \c false otherwise.
+ */
+template <typename T, const unsigned int D, int M>
+bool Vector<T, D, M>::operator!= (const Vector &p) const
+{
+  for(unsigned int d=0; d<D; ++d)
+    if(data[d] != p.data[d])
+      return true;
+ 
+  return false;
+}
+
+/*
+template <typename T, const unsigned int D, int M>
+T Vector<T, D, M>::distance_to(const Vector<T, D, M>& b) const
+{
+  return metric.distance_to(this,b);
+}
+*/
---
+++ vector.h
@@ -0,0 +1,95 @@
+/***************************************************************************
+ *   Copyright (C) 2010-2011 by                                            *
+ *                                                                         *
+ *   Leandro Graciá Gil                                                    *
+ *   leandro.gracia.gil@gmail.com                                          *
+ *                                                                         *
+ *   Juan V. Puertos Ahuir                                                 *
+ *   juanvi.puertos@gmail.com                                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/**
+ * \file 	feature_vector.h
+ * \brief 	Template class for generic D-dimensional feature vectors.
+ * \author	Leandro Graciá Gil, Puertos Ahuir Juan V.
+ * \email       juanvi.puertos@gmail.com
+*/
+
+#ifndef _FEATURE_VECTOR_H_
+#define _FEATURE_VECTOR_H_
+
+#include <functional>
+#include <new>
+
+enum MetricType {
+  Squared,
+};
+
+template <typename T, typename D>
+T Vector<typename T, D, Squared>::distance_to
+
+
+
+template <typename T, const unsigned int D, int M>
+class Metric
+{
+ public:
+  T distance_to(const Vector<T,D,M>& a, const Vector<T,D,M>& b) const
+  {
+    T acc = (T) 0;
+    for (unsigned int i=0; i<D; ++i)
+      acc += (a[i] - b[i]) * (a[i] - b[i]);
+    return acc;
+  }
+};
+
+
+template <typename T, const unsigned int D, int M>
+class Vector
+{
+ public:
+  Vector(); // {}// { metric = new SquaredMetric; }
+  bool operator == (const Vector&) const;   ///< Equality comparison operator.
+  bool operator != (const Vector&) const;   ///< Non-equality comparison operator.
+  void* operator new[] (size_t size); 	///< Standard allocation for arrays of feature vectors.
+  void  operator delete[] (void*); 	///< Standard deallocation for arrays of feature vectors.
+  const T & operator[] (unsigned int i) const { return data[i]; } ///< Const subscript operator.
+  T & operator[] (unsigned int i) { return data[i]; } 		  ///< Subscript 
+  T distance_to(const Vector& b) const
+  {
+    return metric.distance_to(this,b);
+  }
+ private:
+  T data[D];
+  Metric<T,D,M> metric;
+};
+
+/*
+template <typename T, const unsigned int D>
+class VDistance : public std::binary_function<VDistance<T,D>, VDistance<T,D>, bool>
+{
+ public:
+  VDistance();
+  VDistance(unsigned int, T distance);
+  bool operator() (const VDistance<T,D>&, const VDistance<T,D>&) const;
+ private:
+  unsigned int index; ///< Index of the feature vector in the data set.
+  T distance;         ///< Distance of the referenced element to an implicit point.
+};
+*/
+
+#endif
