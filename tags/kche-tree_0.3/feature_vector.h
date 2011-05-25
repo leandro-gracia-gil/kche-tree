a51ead8f384cf308a1a2b322d3f0e608ff0db9ad
leandro.gracia.gil@gmail.com <leandro.gracia.gil@gmail.com@19df2200-dbfd-8bb6-e353-256aed0257e5>
2011-05-25 20:18:04 +0000

    Adding the v0.3 release to tags.

---
+++ tags/kche-tree_0.3/feature_vector.h
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Leandro Graciá Gil                              *
+ *   leandro.gracia.gil@gmail.com                                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/**
+ * \file feature_vector.h
+ * \brief Template for generic D-dimensional feature vectors.
+ * \author Leandro Graciá Gil
+*/
+
+#ifndef _FEATURE_VECTOR_H_
+#define _FEATURE_VECTOR_H_
+
+// Include STL binary predicates.
+#include <functional>
+
+/**
+ * \brief Template for D-dimensional feature vectors.
+ *
+ * Encapsulates a D-dimensional array with an extra entry of the same type for the class label.
+ * Has been compared to the direct use of arrays with no change in efficiency.
+ *
+ * \tparam T Data type of the elements in the vector.
+ * \tparam D Number of dimensions of the vector.
+ */
+template <typename T, const unsigned int D>
+struct feature_vector {
+
+  /// Data array.
+  T data[D];
+
+  // Constructors.
+  feature_vector() {} ///< Default constructor.
+  feature_vector(T value) {
+    for (unsigned int d=0; d<D; ++d)
+      data[d] = value;
+  } ///< Value initialization constructor.
+
+  // Subscript operators.
+  const T & operator [] (unsigned int index) const { return data[index]; } ///< Const subscript operator.
+  T & operator [] (unsigned int index) { return data[index]; } ///< Subscript operator.
+
+  // Comparison operators.
+  bool operator == (const feature_vector &p) const; ///< Equality comparison operator.
+  bool operator != (const feature_vector &p) const; ///< Non-equality comparison operator.
+
+  // Squared distance operators for two D-dimensional points of type T.
+  inline T distance_to(const feature_vector &p) const; ///< Squared distance to a point.
+  inline T distance_to(const feature_vector &p, T upper_bound) const; ///< Squared distance to a point with an upper bound.
+
+  // Memory operators: used to allow memory-aligned specializations. For example, for SSE optimizations.
+  void *operator new [] (size_t size); ///< Standard allocation for arrays of feature vectors.
+  void  operator delete [] (void *p); ///< Standard deallocation for arrays of feature vectors.
+
+} __attribute__((packed));
+
+/**
+ * \brief Vector-distance structure. References a feature vector by its index and its squared distance to another implicit vector.
+ *
+ * Implements its own comparison function with the parenthesis operator for STL-based algorithm use.
+ *
+ * \tparam T  Type used to encode the distance between two feature vectors. Should be the same than the data from the vectors.
+ */
+template <typename T>
+struct vector_distance : public std::binary_function <vector_distance<T>, vector_distance<T>, bool> {
+
+  unsigned int index; ///< Index of the feature vector in the data set.
+  T squared_distance; ///< Squared distance of the referenced element to an implicit point.
+
+  // Default and convenience constructors.
+  vector_distance() {}
+  vector_distance(unsigned int index, T squared_distance) : index(index), squared_distance(squared_distance) {}
+
+  /// Distance comparison operator for vector_distances. Allows vector_distance objects to be used as STL comparison functors.
+  bool operator () (const vector_distance &v1, const vector_distance &v2) const {
+    return v1.squared_distance < v2.squared_distance;
+  }
+};
+
+// Template implementation.
+#include "feature_vector.cpp"
+
+#endif
---
+++ tags/kche-tree_0.3/k-vector.cpp
@@ -0,0 +1,166 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Leandro Graciá Gil                              *
+ *   leandro.gracia.gil@gmail.com                                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/**
+ * \file k-vector.cpp
+ * \brief Template implementations for k-vectors holding the best k elements (linear, but simpler).
+ * \author Leandro Graciá Gil
+*/
+
+/**
+ * Build a k-vector of size K.
+ *
+ * \param K Size of the k-vector.
+ * \param c Comparison object used for internal element sorting.
+ */
+template <typename T, typename Compare>
+k_vector<T, Compare>::k_vector(unsigned int K, const Compare &c)
+  : data(new T[K]),
+    K(K),
+    stored(0),
+    compare(c) {}
+
+/** Default destructor */
+template <typename T, typename Compare>
+k_vector<T, Compare>::~k_vector() {
+  delete []data;
+}
+
+/**
+ * Check if no elements are being stored in the k-vector.
+ *
+ * \return \c true if the k-vector is empty, \c false otherwise.
+ */
+template <typename T, typename Compare>
+bool k_vector<T, Compare>::empty() const {
+  return stored == 0;
+}
+
+/**
+ * Check if K elements are being stored in the k-vector with K being the maximum size.
+ *
+ * \return \c true if the k-vector is full, \c false otherwise.
+ */
+template <typename T, typename Compare>
+bool k_vector<T, Compare>::full() const {
+  return stored == K;
+}
+
+/**
+ * Get the current number of elements in the k-vector.
+ *
+ * \return Number of elements currently in the k-vector.
+ */
+template <typename T, typename Compare>
+unsigned int k_vector<T, Compare>::size() const {
+  return stored;
+}
+
+/**
+ * Get the worst element currently stored in the k-vector.
+ *
+ * \return Reference to the worst element stored.
+ */
+template <typename T, typename Compare>
+const T & k_vector<T, Compare>::front() const {
+  return data[0];
+}
+
+/**
+ * Get the best element currently stored in the k-vector.
+ *
+ * \return Reference to the best element stored.
+ */
+template <typename T, typename Compare>
+const T & k_vector<T, Compare>::back() const {
+  return data[stored - 1];
+}
+
+/**
+ * Pop the best element stored in the k-vector.
+ */
+template <typename T, typename Compare>
+void k_vector<T, Compare>::pop_back() {
+  if (stored > 0)
+    --stored;
+}
+
+/**
+ * Push a new element into the k-vector.
+ *
+ * \param elem Element being pushed.
+ */
+template <typename T, typename Compare>
+void k_vector<T, Compare>::push_back(const T &elem) {
+  if (stored < K)
+    push_not_full(elem);
+  else
+    push_full(elem);
+}
+
+/**
+ * Push a new element into the k-vector with the precondition that it's still not full.
+ * Ensuring the precondition before calling is responsability of the caller.
+ * For a generic pushing method use \link k_vector::push_back push_back\endlink.
+ *
+ * \pre The k-vector object must not be full.
+ *
+ * \param elem Element being pushed.
+ */
+template <typename T, typename Compare>
+void k_vector<T, Compare>::push_not_full(const T &elem) {
+
+  // Look where the new candidate should be placed.
+  unsigned int index;
+  for (index=0; index < stored && compare(elem, data[index]); ++index);
+
+  // Move later candidates and store the new one in its place.
+  for (unsigned int i=stored; i>index; --i)
+    data[i] = data[i-1];
+  data[index] = elem;
+  ++stored;
+}
+
+/**
+ * Push a new element into the k-vector with the precondition that it's already full.
+ * Ensuring the precondition before calling is responsability of the caller.
+ * For a generic pushing method use \link k_vector::push_back push_back\endlink.
+ *
+ * \pre The k-vector object must be already full.
+ *
+ * \param elem Element being pushed.
+ */
+template <typename T, typename Compare>
+void k_vector<T, Compare>::push_full(const T &elem) {
+
+  // Avoid further calculations if candidate is worst than the current worst one.
+  if (!compare(elem, data[0]))
+    return;
+
+  // Look where the new candidate should be placed.
+  unsigned int index;
+  for (index=1; index < stored && compare(elem, data[index]); ++index);
+
+  // Move previous candidates and store the new one in its place.
+  --index;
+  for (unsigned int i=0; i<index; ++i)
+    data[i] = data[i+1];
+  data[index] = elem;
+}
