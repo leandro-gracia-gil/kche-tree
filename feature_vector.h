ded96c1d796dab8ee7968e4104d557b71cd5b844
hargtholan@gmail.com <hargtholan@gmail.com@19df2200-dbfd-8bb6-e353-256aed0257e5>
2011-05-29 00:19:49 +0000

    Author added, I got to make performance testing.

--- feature_vector.h
+++ feature_vector.h
@@ -21,7 +21,7 @@
 /**
  * \file feature_vector.h
  * \brief Template for generic D-dimensional feature vectors.
- * \author Leandro Graciá Gil
+ * \author Leandro Graciá Gil, Juan V. Puertos
 */
 
 #ifndef _FEATURE_VECTOR_H_
